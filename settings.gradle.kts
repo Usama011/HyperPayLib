f9a9936abc8950bc846fbacf62a2f4b1ddf5cc93
Ahmad Usama Tariq <cadetusama011@gmail.com>
2023-10-16 21:30:44 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HyperPayLib"
+include(":app")
+include(":app:hyperpay")
