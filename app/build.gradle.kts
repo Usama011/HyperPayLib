0ba26268c6a7c4a563cc9d3368658298587ad85b
Ahmad Usama Tariq <cadetusama011@gmail.com>
2023-10-24 15:48:40 +0500

    Updated 1.0.2 app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.android"
             artifactId = "hyperpaylib"
-            version = "1.0.1"
+            version = "1.0.2"
 //            afterEvaluate {
 //                from(components["release"])
 //            }
